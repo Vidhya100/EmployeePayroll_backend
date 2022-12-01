64283b9e3efd6126dd0f38bbf2724c3b6ef4e2c6
Vidhya100 <vidhyadarade1@gmail.com>
2022-12-01 16:41:42 +0530

    project setup and model file

---
+++ ModelLayer/Service/EmpModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelLayer.Service
+{
+    public class EmpModel
+    {
+        [Key]
+        public int EmpID { get; set; }
+        public string EmpName { get; set; }
+        public string ProfileImg { get; set; }
+        public string Gender { get; set; }
+        public string Department { get; set; }
+        public double Salary { get; set; }
+        public DateTime StartDate { get; set; }
+        public string Notes { get; set; }
+    }
+}
---
+++ RepositoryLayer/Service/UserRL.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace RepositoryLayer.Service
+{
+    public class UserRL  : IUserRL
+    {
+        private readonly IConfiguration iconfiguration;
+
+        public UserRL(IConfiguration iconfiguration)
+        {
+            this.iconfiguration = iconfiguration;
+        }
+
+    }
+}
