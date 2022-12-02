6104f2bd7b292cff43b12b4ddf42700c41e8d53c
Vidhya100 <vidhyadarade1@gmail.com>
2022-12-02 15:04:21 +0530

    controller and view for get all employees

---
+++ EmployeePayrolMVC/Controllers/EmpController.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLayer.Service;
+using Microsoft.AspNetCore.Mvc;
+using ModelLayer.Service;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EmployeePayrolMVC.Controllers
+{
+    public class EmpController : Controller
+    {
+        IUserBL iuserBL;
+
+        public EmpController(IUserBL iuserBL)
+        {
+            this.iuserBL = iuserBL;
+        }
+        public IActionResult GetAllEmployees()
+        {
+            List<EmpModel> lstEmployee = new List<EmpModel>();
+            lstEmployee = iuserBL.GetAllEmployees().ToList();
+
+            return View(lstEmployee);
+        }
+    }
+}
